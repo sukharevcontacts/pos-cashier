31b3aa36870ad7dde23ae917a0a2a7bdd2751f3e
AleksandGorbachev <90742867+AleksandGorbachev@users.noreply.github.com>
2026-05-14 17:22:32 +0700

    pos-2 удалил ненужные роуты

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -2,15 +2,10 @@ from fastapi import APIRouter
 
 from app.api import (
     auth,
-    stores,
     users,
     orders,
     items,
-    order_items,
-    order_payment,
     order_receipt,
-    topup,
-    stock,
     transactions,
     cashier_settings,
     status,
@@ -21,15 +16,10 @@ from app.api import (
 api_router = APIRouter(prefix="/api")
 
 api_router.include_router(auth.router)
-api_router.include_router(stores.router)
 api_router.include_router(users.router)
 api_router.include_router(orders.router)
 api_router.include_router(items.router)
-api_router.include_router(order_items.router)
-api_router.include_router(order_payment.router)
 api_router.include_router(order_receipt.router)
-api_router.include_router(topup.router)
-api_router.include_router(stock.router)
 api_router.include_router(transactions.router)
 api_router.include_router(cashier_settings.router)
 api_router.include_router(status.router)
